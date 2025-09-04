a4f92b22834751eee49cbea2646ccd7f58517fde
fsandic <fsandich@gmail.com>
2025-09-04 10:50:10 -0600

    Poner logs para revisar

--- CrearRFCVulnerabilidades.js
+++ CrearRFCVulnerabilidades.js
@@ -1,50 +1,63 @@
 (function executeBulkRFC() {
-    // Captura los sys_id seleccionados del contexto de lista
     var selectedSysIds = (typeof action !== 'undefined' && typeof action.getGlideListSelection === 'function') ? action.getGlideListSelection() : [];
+    gs.log('RFC Grupal: sys_id seleccionados = ' + selectedSysIds);
+
     if (!selectedSysIds || selectedSysIds.length === 0) {
         gs.addErrorMessage('Debes seleccionar vulnerabilidades.');
+        gs.log('RFC Grupal: No hay vulnerabilidades seleccionadas.');
         return;
     }
 
     var description = 'Se realizará la remediación de las vulnerabilidades:\n';
     var origenList = [];
+    var registroProcesado = false;
 
     for (var i = 0; i < selectedSysIds.length; i++) {
-        var vulnItem = new GlideRecord('sn_vul_vulnerable_item');
+        var vulnItem = new GlideRecord('sn_vul_vulnerable_item'); // Confirma el nombre exacto de la tabla
         if (vulnItem.get(selectedSysIds[i])) {
-            // Agregar el número del registro ("Number") a la descripción
+            registroProcesado = true;
             description += vulnItem.number + '\n';
 
-            // Obtener el nombre/código de la vulnerabilidad (referencia)
-            // Si el campo es referencia a 'sn_vul_vulnerability', adáptalo aquí
-            var vulnCode = '';
+            // Obtención del código de vulnerabilidad
             if (vulnItem.vulnerability) {
-                var vulnRec = new GlideRecord('sn_vul_vulnerability');
-                if (vulnRec.get(vulnItem.vulnerability)) {
-                    vulnCode = vulnRec.name + '';
-                    // Quitar 'TEN-' si existe y dejar solo el número
+                var vulnRec = new GlideRecord('sn_vul_vulnerability'); // Confirma nombre exacto
+                if (vulnRec.get(vulnItem.vulnerability.toString())) {
+                    var vulnCode = vulnRec.name + '';
                     if (vulnCode.indexOf('TEN-') === 0) {
-                        vulnCode = vulnCode.substring(4); // Elimina los primeros 4 caracteres (TEN-)
+                        vulnCode = vulnCode.substring(4);
                     }
                     origenList.push(vulnCode);
+                } else {
+                    gs.log('RFC Grupal: No se encontró vulnerabilidad ' + vulnItem.vulnerability.toString());
                 }
             }
+        } else {
+            gs.log('RFC Grupal: No se encontró vulnerable_item ' + selectedSysIds[i]);
         }
     }
 
-    // Crear el RFC estándar con los datos indicados
+    if (!registroProcesado) {
+        gs.addErrorMessage('No se pudo procesar ningún registro!');
+        gs.log('RFC Grupal: Ningún registro procesado.');
+        return;
+    }
+
+    // Creación del RFC
     var rfc = new GlideRecord('change_request');
     rfc.initialize();
-    rfc.type = 'standard'; // RFC tipo estándar
+    rfc.type = 'standard';
     rfc.short_description = 'RFC grupal de remediación';
     rfc.description = description;
-    rfc.u_origen = origenList.join(','); // Campo personalizado con códigos separados por coma
-
+    rfc.u_origen = origenList.join(','); // Campo personalizado
     var newRFCId = rfc.insert();
+    gs.log('RFC Grupal: sys_id RFC creado = ' + newRFCId);
+
     if (newRFCId) {
         gs.addInfoMessage('RFC creado correctamente');
-        action.setRedirectURL('/change_request.do?sys_id=' + newRFCId); // Redirección a RFC creado
+        action.setRedirectURL('/change_request.do?sys_id=' + newRFCId);
     } else {
         gs.addErrorMessage('No se pudo crear el RFC');
+        gs.log('RFC Grupal: Error al crear RFC.');
     }
 })();
+
