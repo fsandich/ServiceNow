6c3223848bba0e2b2d214e883d643de389dd4bcf
fsandich <91922747+fsandich@users.noreply.github.com>
2025-09-04 13:02:31 -0600

    Update errores.js

--- errores.js
+++ errores.js
@@ -1,3 +1,8 @@
 Uncaught ReferenceError: g_list is not defined
     at onClick (sn_vul_vulnerable_item_list.do:550:26)
     at sn_vul_vulnerable_item_list.do:566:1
+
+function onClick() {
+                                            console.log('RFC Grupal: Client Script iniciado');
+                                            // Log en consola del navegador
+                                            var selectedSysIds = g_list.getChecked();
